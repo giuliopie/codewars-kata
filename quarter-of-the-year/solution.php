9bef9c4c5df6fd14e26e9d911ac5a8f376bdea9e
giuliopie <giuliopiepoli.dev@gmail.com>
2022-05-06 22:44:10 +0200

    Publish solution for Kata

---
+++ quarter-of-the-year/solution.php
@@ -0,0 +1,8 @@
+<?php
+function quarterOf($month)
+{
+    if ($month >= 1 && $month < 4) return 1;
+    if ($month >= 4 && $month < 7) return 2;
+    if ($month >= 7 && $month < 10) return 3;
+    if ($month >= 10 && $month <= 12) return 4;
+}
